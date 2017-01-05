64d89b60285ec8f6a1cffb406377dcd91cbc015a
Misha Moroshko <michael.moroshko@gmail.com>
2017-01-04 23:26:16 -0800

    Draw proper ellipses arcs

--- src/Line/Line.js
+++ src/Line/Line.js
@@ -1,19 +1,41 @@
 import React, { Component } from 'react';
 
 const LINE_COLOR = '#000';
+const LINE_WIDTH = '5';
 
 export default class Line extends Component {
   render() {
     const { startRow, startColumn, endRow, endColumn, cellSize, isStraight } = this.props;
+    const startX = cellSize * (startColumn + 0.5);
+    const startY = cellSize * (startRow + 0.5);
+    const endX = cellSize * (endColumn + 0.5);
+    const endY = cellSize * (endRow + 0.5);
+
+    if (isStraight) {
+      return (
+        <line
+          x1={startX}
+          y1={startY}
+          x2={endX}
+          y2={endY}
+          stroke={LINE_COLOR}
+          strokeWidth={LINE_WIDTH}
+        />
+      );
+    }
+
+    const dx = endX - startX;
+    const dy = endY - startY;
+    const rx = Math.sqrt(dx * dx + dy * dy) / 2;
+    const ry = cellSize;
+    const xAxisRotation = Math.atan2(dy, dx) * 180 / Math.PI; // Magic here :)
 
     return (
-      <line
-        x1={cellSize * (startColumn + 0.5)}
-        y1={cellSize * (startRow + 0.5)}
-        x2={cellSize * (endColumn + 0.5)}
-        y2={cellSize * (endRow + 0.5)}
-        stroke={isStraight ? LINE_COLOR : '#f00'}
-        strokeWidth="5"
+      <path
+        d={`M${startX} ${startY} a ${rx} ${ry} ${xAxisRotation} 0 0 ${dx} ${dy}`}
+        stroke={LINE_COLOR}
+        strokeWidth={LINE_WIDTH}
+        fill="transparent"
       />
     );
   }
